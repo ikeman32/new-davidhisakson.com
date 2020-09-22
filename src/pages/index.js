0bec08ab5cb5d0b35fa0233b17f4421e2667673e
David Isakson <ikeman32@yahoo.com>
2020-09-22 13:42:09 -0700

    Begin Blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import Home from './Home';
 import About from './About';
 import Projects from './Projects';
 import ProjectCards from './cards/ProjectCards'
+import Blog from './blog/Blog';
 
 export{
     Home,
     About,
     Projects,
     ProjectCards,
+    Blog
 }
