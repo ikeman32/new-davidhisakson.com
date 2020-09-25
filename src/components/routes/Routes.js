482800852911546fdbfc4c84b2ad4d2d82c334ab
David Isakson <ikeman32@yahoo.com>
2020-09-25 10:25:47 -0700

    Change routes

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Home, About, Projects } from '../../pages';
+import { Home, About, Projects, Blog } from '../../pages';
 
 const Routes = () => {
     return (
@@ -8,6 +8,7 @@ const Routes = () => {
             <Route exact path="/" component={Home} />
             <Route path='/Projects' component={Projects} />
             <Route path='/About' component={About} />
+            <Route path='/Blog' component={Blog} />
             {/* 
             <Route path='/Videos' component={Videos} />
             
